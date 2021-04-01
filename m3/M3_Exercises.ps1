75e6db8944ce58610302c3118b060e707dfe0382
Daerzk <63276869+Daerzk@users.noreply.github.com>
2021-04-01 13:20:54 +1100

    Custom Role Assignment

--- m3/M3_Exercises.ps1
+++ m3/M3_Exercises.ps1
@@ -25,7 +25,7 @@ $keyVaultParameters = @{
 $keyVault = New-AzKeyVault @keyVaultParameters
 
 #If you already have a Key Vault
-$keyVault = Get-AzKeyVault -VaultName "VAULT_NAME" -ResourceGroupName "RESOURCE_GROUP_NAME"
+$keyVault = Get-AzKeyVault -VaultName "cmk-key-vault-7182" -ResourceGroupName "LAB-RG"
 
 #Get the existing custom roles
 Get-AzRoleDefinition | Where-Object {$_.IsCustom -eq $True} | Format-Table Name, IsCustom
@@ -37,10 +37,10 @@ $roleInfo = Get-Content .\custom_role.json
 
 $roleInfo -replace "SUBSCRIPTION_ID",$subId > updated_role.json
 
-$role = New-AzRoleDefinition -InputFile .\updated_role.json
+$role = New-AzRoleDefinition -InputFile .\m3\custom_role.json
 
 #Assign the custom role to an existing user
-$user = Get-AzADUser -UserPrincipalName "USER_PRINCIPAL_NAME"
+$user = Get-AzADUser -UserPrincipalName "chris.martyn@daerzk.com"
 
 $assignmentInfo = @{
     ObjectId = $user.Id
